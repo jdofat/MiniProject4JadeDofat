cce7490872c70ee1ec94501d39e167700dec4d79
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-11-30 09:32:55 -0500

    Update wsgi.py

--- internshipfinder/wsgi.py
+++ internshipfinder/wsgi.py
@@ -1,3 +1,10 @@
+# Django WSGI Overview: https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+# get_wsgi_application Reference: https://docs.djangoproject.com/en/5.2/ref/applications/#django.core.wsgi.get_wsgi_application
+# Environment Variables in Django: https://docs.djangoproject.com/en/5.2/topics/settings/#envvar-DJANGO_SETTINGS_MODULE
+# Django Deployment Guide: https://docs.djangoproject.com/en/5.2/howto/deployment/
+# WSGI vs ASGI Explanation: https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+
+
 import os
 from django.core.wsgi import get_wsgi_application
 
