e60080774c056d1973328f4680b74368d6917ee9
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-11-30 09:39:26 -0500

    Update views.py

--- internships/views.py
+++ internships/views.py
@@ -1,3 +1,31 @@
+# django views:
+# https://docs.djangoproject.com/en/5.2/topics/http/views/
+
+# django render:
+# https://docs.djangoproject.com/en/5.2/intro/tutorial03/#write-views-that-actually-do-something
+
+# get_object_or_404 :
+# https://docs.djangoproject.com/en/5.2/topics/http/shortcuts/#get-object-or-404
+
+# get and post:
+# https://docs.djangoproject.com/en/5.2/topics/forms/#the-view
+
+# models/filtering:
+# https://docs.djangoproject.com/en/5.2/topics/db/queries/#retrieving-specific-objects-with-filters
+
+# field lookups:
+# https://docs.djangoproject.com/en/5.2/ref/models/querysets/#icontains
+
+#  context -- > templates:
+# https://docs.djangoproject.com/en/5.2/intro/tutorial03/#passing-a-context
+
+# pk lookup:
+# https://docs.djangoproject.com/en/5.2/intro/tutorial03/#a-shortcut-get-object-or-404
+
+
+# ----------------------------------------------------
+
+
 from django.shortcuts import render, get_object_or_404
 from .models import Internship
 
