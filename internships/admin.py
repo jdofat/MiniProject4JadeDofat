46da05649f04df1509fda09da2286ac2e4678523
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-11-30 09:35:13 -0500

    Update admin.py

--- internships/admin.py
+++ internships/admin.py
@@ -1,3 +1,8 @@
+# django admin site: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/
+# modeladmin reference: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#modeladmin-objects
+# registering models in admin: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#django.contr
+
+
 from django.contrib import admin
 from .models import Internship
 
