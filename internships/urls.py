003dce6178ea2da26d3bba18e5c3271db634d601
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-11-30 09:37:32 -0500

    Update urls.py

--- internships/urls.py
+++ internships/urls.py
@@ -1,3 +1,7 @@
+# django urls and path: https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# url converters (like int:pk): https://docs.djangoproject.com/en/5.2/topics/http/urls/#path-converters
+# views in urls: https://docs.djangoproject.com/en/5.2/intro/tutorial03/#writing-more-views
+
 from django.urls import path
 from . import views
 
