3256587dc67dc99a005fbf3ff3a5dda37d85e28e
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-11-30 09:33:48 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,10 @@
+# Django manage.py : https://docs.djangoproject.com/en/5.2/ref/django-admin/
+# execute_from_command_line Reference: https://docs.djangoproject.com/en/5.2/ref/django-admin/#django.core.management.execute_from_command_line
+# DJANGO_SETTINGS_MODULE Environment Variable: https://docs.djangoproject.com/en/5.2/topics/settings/#envvar-DJANGO_SETTINGS_MODULE
+# Understanding sys.argv in Python : https://docs.python.org/3/library/sys.html#sys.argv
+# Python os module documentation: https://docs.python.org/3/library/os.html
+
+
 import os
 import sys
 
