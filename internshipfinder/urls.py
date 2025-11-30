7e656a4c3f07ef1e33cd8cd38f4b7adc079b5a0a
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-11-30 09:31:58 -0500

    Update urls.py

--- internshipfinder/urls.py
+++ internshipfinder/urls.py
@@ -1,3 +1,24 @@
+# https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# Django URL dispatcher docs^
+
+# https://docs.djangoproject.com/en/6.0/intro/tutorial01/
+# Django “Writing your first app” tutorial (urls.py setup)^ 
+
+# https://docs.djangoproject.com/en/4.0/ref/urls/
+# Reference for django.urls.path() and include() ^ 
+
+# https://www.geeksforgeeks.org/django-url-patterns-python/        
+# GeeksforGeeks overview of Django URLConf / urlpatterns ^
+
+
+# https://www.w3schools.com/django/django_urls.php                 
+# W3Schools Django tutorial for setting up urls.py ^
+
+
+# https://django-tutorial.dev/course/django-for-beginners/project-structure/urlspy/
+# Third-party tutorial on Django project URL structure  ^
+
+
 from django.contrib import admin
 from django.urls import path, include
 
