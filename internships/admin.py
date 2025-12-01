4eec227c8146fe6a5922a6ed072ac539ee61cfc6
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-12-01 16:40:35 -0500

    Update admin.py

--- internships/admin.py
+++ internships/admin.py
@@ -1,9 +1,9 @@
 # django admin site: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/
 # modeladmin reference: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#modeladmin-objects
+
+
 # registering models in admin: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#django.contr
-
-
-
+# amin python file to customize how models are displayed/ managed
 from django.contrib import admin
 from .models import Internship
 #what columns show up in the admin list:
