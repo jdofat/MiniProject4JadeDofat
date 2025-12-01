9f1b98a773f7f2310f73cc2b6f203ad0fca9d281
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-12-01 16:39:35 -0500

    Update admin.py

--- internships/admin.py
+++ internships/admin.py
@@ -3,11 +3,12 @@
 # registering models in admin: https://docs.djangoproject.com/en/5.2/ref/contrib/admin/#django.contr
 
 
+
 from django.contrib import admin
 from .models import Internship
-
+#what columns show up in the admin list:
 class InternshipAdmin(admin.ModelAdmin):
     list_display = ["title", "company", "location"]
-
+# display internship model using custom layout from
 admin.site.register(Internship, InternshipAdmin)
 
