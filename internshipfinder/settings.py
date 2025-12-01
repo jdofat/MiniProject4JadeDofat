764b3f1db709e3811b269737b8c4e8789fbb582e
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-12-01 16:34:13 -0500

    Update settings.py

--- internshipfinder/settings.py
+++ internshipfinder/settings.py
@@ -4,12 +4,6 @@
 #  project structure (where settings.py comes from)
 # https://docs.djangoproject.com/en/stable/intro/tutorial01/#creating-a-project
 
-#  INSTALLED_APPS 
-# https://docs.djangoproject.com/en/stable/ref/settings/#installed-apps
-
-#  MIDDLEWARE documentation
-# https://docs.djangoproject.com/en/stable/ref/middleware/
-
 #  SQLite setup
 # https://docs.djangoproject.com/en/stable/ref/settings/#databases
 
@@ -23,11 +17,17 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# https://docs.djangoproject.com/en/5.2/ref/settings/
+# so django will show detailed error pages:
+
 SECRET_KEY = 'dev-secret-key'
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+#  INSTALLED_APPS : which apps the project is using
+# https://docs.djangoproject.com/en/stable/ref/settings/#installed-apps
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'internships',
 ]
+
+#  MIDDLEWARE for request/response processing
+# https://docs.djangoproject.com/en/stable/ref/middleware/
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,7 +53,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'internshipfinder.urls'
 
-#  TEMPLATES 
+#  TEMPLATES: for connecting data & page rendering-- presentation of my site
 # https://docs.djangoproject.com/en/stable/ref/settings/#templates
 
 TEMPLATES = [
@@ -69,6 +72,9 @@ TEMPLATES = [
     },
 ]
 
+
+# https://www.reddit.com/r/learnpython/comments/1dboq9h/i_am_not_able_to_understand_what_is_wsgi_and_why/
+# handle HTTP requests to give backend a framwork in a format it can process
 WSGI_APPLICATION = 'internshipfinder.wsgi.application'
 
 DATABASES = {
@@ -77,6 +83,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# password rules, language and time zone
+# https://docs.allauth.org/en/dev/account/configuration.html
+# https://www.loopwerk.io/articles/2025/django-i18n-date-formats/
 
 AUTH_PASSWORD_VALIDATORS = []
 
@@ -95,4 +105,5 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
+# allows database tables to handle more records
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
